b003f64508e3f1271fd4d59d88bd85d2f3790675
ykxiao <xchris80519@gmail.com>
2025-03-18 22:34:05 +0800

    訂單功能 - 檢查User是否存在、檢查商品庫存

--- src/main/java/com/xchris/springbootmall/service/impl/OrderServiceImpl.java
+++ src/main/java/com/xchris/springbootmall/service/impl/OrderServiceImpl.java
@@ -2,13 +2,17 @@ package com.xchris.springbootmall.service.impl;
 
 import com.xchris.springbootmall.dao.OrderDao;
 import com.xchris.springbootmall.dao.ProductDao;
+import com.xchris.springbootmall.dao.UserDao;
 import com.xchris.springbootmall.model.Order;
 import com.xchris.springbootmall.model.OrderItem;
 import com.xchris.springbootmall.model.Product;
+import com.xchris.springbootmall.model.User;
 import com.xchris.springbootmall.service.OrderService;
 import dto.BuyItem;
 import dto.CreateOrderRequest;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,18 +21,23 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class OrderServiceImpl implements OrderService {
+    // log
+    private final static Logger log = LoggerFactory.getLogger(OrderServiceImpl.class);
+
 
     @Autowired
     private ProductDao productDao;
-
     @Autowired
     private OrderDao orderDao;
+    @Autowired
+    private UserDao userDao;
 
     @Override
     public Order getOrderById(Integer orderId) {
@@ -42,11 +51,34 @@ public class OrderServiceImpl implements OrderService {
     @Transactional
     @Override
     public Integer createOrder(Integer userId, CreateOrderRequest createOrderRequest) {
+        // 檢查User是否存在
+        User user = userDao.getUserById(userId);
+        if(user == null) {
+            log.warn("此 user ID: {} 不存在");
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
+
+
         int totalAmount = 0;
         List<OrderItem> orderItemList = new ArrayList<>();
 
         for(BuyItem buyItem : createOrderRequest.getBuyItemList()){
             Product product = productDao.getProductById(buyItem.getProductId());
+
+            // 檢查商品是否存在、庫存是否足夠
+            if(product == null) {
+                log.warn("商品 {} 不存在", buyItem.getProductId());
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+            }else if(product.getStock() < buyItem.getQuantity()){
+                log.warn("商品 {} 庫存量不足，無法購買。 剩餘庫存 {}， 欲購買數量 {}",
+                        buyItem.getProductId(),product.getStock(),buyItem.getQuantity());
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+            }
+
+            //扣除商品庫存
+            productDao.updateStock(product.getProductId(), product.getStock() - buyItem.getQuantity());
+
 
             // 計算總價錢
             int amount = buyItem.getQuantity() * product.getPrice();
